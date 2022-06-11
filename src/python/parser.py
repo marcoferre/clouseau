f9d5f74d292d4b3673ff05e26026ed588d5822a4
marcoferre <10467480@polimi.it>
2022-06-11 18:24:44 +0200

    refactor to avoid code repetitions

--- src/python/parser.py
+++ src/python/parser.py
@@ -10,8 +10,6 @@ aximaster_list = []
 BASE_ADDRESS = 0x10
 
 for line in lines:
-    if line[0] == "\n":
-        continue
     if line[0:2] == "//":
         continue
 
@@ -19,26 +17,22 @@ for line in lines:
     if "#pragma HLS INTERFACE" in line:
         params = {}
         parts = line.split()
+        # parse all parameters
+        for el in parts[4:]:
+            #TODO parse also parameters without =
+            key, value = el.split('=')
+            params[key] = value
+
         #if is axi master definition
         if parts[3] == "m_axi":
-            #parse all parameters
-            for el in parts[4:]:
-                key, value = el.split('=')
-                params[key] = value
             aximaster_list.append(params)
 
         #if is axi lite definition
         elif parts[3] == "s_axilite":
-            #parse all parameters
-            for el in parts[4:]:
-                key, value = el.split('=')
-                params[key] = value
             #if is not return ip compute the address for each axilite
             if params['port'] != 'return':
                 params['address'] = hex(BASE_ADDRESS)
                 BASE_ADDRESS += 0x8
-
             axilite_list.append(params)
-
 
 print(axilite_list)
