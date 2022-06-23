8e2c852a0ffb869dda18173fd847fce4329ab5df
marco <10467480@polimi.it>
2022-06-23 15:27:49 +0200

    compute all function

--- src/hls/test/iron/mutual_info.cpp
+++ src/hls/test/iron/mutual_info.cpp
@@ -1,31 +1,3 @@
-/******************************************
-*MIT License
-*
-# *Copyright (c) [2020] [Davide Conficconi, Eleonora D'Arnese, Emanuele Del Sozzo, Donatella Sciuto, Marco Domenico Santambrogio]
-*
-*Permission is hereby granted, free of charge, to any person obtaining a copy
-*of this software and associated documentation files (the "Software"), to deal
-*in the Software without restriction, including without limitation the rights
-*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*copies of the Software, and to permit persons to whom the Software is
-*furnished to do so, subject to the following conditions:
-*
-*The above copyright notice and this permission notice shall be included in all
-*copies or substantial portions of the Software.
-*
-*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-*SOFTWARE.
-*/
-/***************************************************************
-*
-* High-Level-Synthesis implementation file for Mutual Information computation
-*
-****************************************************************/
 #include "histogram.h"
 #include "entropy.h"
 #include <stdio.h>
@@ -50,11 +22,16 @@ typedef enum FUNCTION_T {
 } FUNCTION;
 
 
-void compute(INPUT_DATA_TYPE * input_img, INPUT_DATA_TYPE * input_ref,  data_t * mutual_info){
+void iron(INPUT_DATA_TYPE * input_img, INPUT_DATA_TYPE * input_ref,  data_t * mutual_info){
 
-#ifndef CACHING
-	#pragma HLS INLINE
-#endif
+#pragma HLS INTERFACE m_axi port=input_img depth=fifo_in_depth offset=slave bundle=gmem0
+#pragma HLS INTERFACE m_axi port=input_ref depth=fifo_in_depth offset=slave bundle=gmem1
+#pragma HLS INTERFACE m_axi port=mutual_info depth=1 offset=slave bundle=gmem2
+
+#pragma HLS INTERFACE s_axilite port=input_img bundle=control
+#pragma HLS INTERFACE s_axilite port=input_ref bundle=control
+#pragma HLS INTERFACE s_axilite port=mutual_info register bundle=control
+#pragma HLS INTERFACE s_axilite port=return bundle=control
 
 #pragma HLS DATAFLOW
 
@@ -154,88 +131,3 @@ static	hls::stream<data_t> mutual_information_stream("mutual_information_stream"
 	stream2axi<data_t, fifo_out_depth>(mutual_info, mutual_information_stream);
 
 }
-
-
-template<typename T, unsigned int size>
-void copyData(T* in, T* out){
-	for(int i = 0; i < size; i++){
-#pragma HLS PIPELINE
-		out[i] = in[i];
-	}
-}
-
-
-#ifndef CACHING
-
-#ifdef KERNEL_NAME
-extern "C"{
-	void KERNEL_NAME
-#else
-	void mutual_information_master
-#endif //KERNEL_NAME
-(INPUT_DATA_TYPE * input_img, INPUT_DATA_TYPE * input_ref, data_t * mutual_info){
-#pragma HLS INTERFACE m_axi port=input_img depth=fifo_in_depth offset=slave bundle=gmem0
-#pragma HLS INTERFACE m_axi port=input_ref depth=fifo_in_depth offset=slave bundle=gmem1
-#pragma HLS INTERFACE m_axi port=mutual_info depth=1 offset=slave bundle=gmem2
-
-#pragma HLS INTERFACE s_axilite port=input_img bundle=control
-#pragma HLS INTERFACE s_axilite port=input_ref bundle=control
-#pragma HLS INTERFACE s_axilite port=mutual_info register bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-
-	compute(input_img, input_ref, mutual_info);
-
-}
-
-
-
-#else // CACHING
-
-
-#ifdef KERNEL_NAME
-extern "C"{
-	void KERNEL_NAME
-#else
-	void mutual_information_master
-#endif //KERNEL_NAME
-(INPUT_DATA_TYPE * input_img,  data_t * mutual_info, unsigned int functionality, int *status){
-#pragma HLS INTERFACE m_axi port=input_img depth=fifo_in_depth offset=slave bundle=gmem0
-#pragma HLS INTERFACE m_axi port=mutual_info depth=1 offset=slave bundle=gmem2
-#pragma HLS INTERFACE m_axi port=status depth=1 offset=slave bundle=gmem1
-
-#pragma HLS INTERFACE s_axilite port=input_img bundle=control
-#pragma HLS INTERFACE s_axilite port=mutual_info register bundle=control
-#pragma HLS INTERFACE s_axilite port=functionality register bundle=control
-#pragma HLS INTERFACE s_axilite port=status register bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-
-
-	static INPUT_DATA_TYPE ref_img[NUM_INPUT_DATA] = {0};
-
-#ifdef URAM
-#pragma HLS RESOURCE variable=ref_img core=RAM_1P_URAM
-#endif //URAM
-
-	switch(functionality){
-	case LOAD_IMG:	copyData<INPUT_DATA_TYPE, NUM_INPUT_DATA>(input_img, ref_img);
-					*status = 1;
-					*mutual_info = 0.0;
-					break;
-	case COMPUTE:	compute(input_img, ref_img, mutual_info);
-					*status = 1;
-					break;
-	default:		*status = -1;
-					*mutual_info = 0.0;
-					break;
-	}
-
-
-}
-
-#endif //CACHING
-
-#ifdef KERNEL_NAME
-
-} // extern "C"
-
-#endif //KERNEL_NAME
